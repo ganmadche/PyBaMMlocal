e18c5fa84689864e1e94fc14777cd1671f4d1131
Ganesh Madabattula <ganesh.che@iitbhu.ac.in>
2026-03-10 09:56:40 +0000

    retuned std300 cycles for 500cycles

--- wip/std300cycles_tuneparameters.py
+++ wip/std300cycles_tuneparameters.py
@@ -6,7 +6,7 @@ parameter_values.update({"Negative electrode OCP entropic change [V.K-1]": -0.00
                          "Contact resistance [Ohm]": 0.001,
                         })
 def ec_diffusivity(T_amb):
-    D_sj = 2.5e-22*4*10*3 
+    D_sj = 2.5e-22*4*10*3*2 
     EaD = 10000
     arrhenius = np.exp(EaD / pybamm.constants.R *(1/ 298.15 - 1/ T_amb))
     return D_sj * arrhenius
@@ -20,7 +20,7 @@ parameter_values.update({"SEI growth activation energy [J.mol-1]": 48000.0,
 # LAM parameters update
 
 parameter_values.update({"Positive electrode LAM constant proportional term [s-1]": 0.1*0.5*1.5/3600,
-                         "Negative electrode LAM constant proportional term [s-1]": 0.5*0.5*1.5/3600,
+                         "Negative electrode LAM constant proportional term [s-1]": 0.2*0.5*1.5/3600,
                          
                          "Positive electrode LAM constant exponential term": 2.0*1.1,
                          "Negative electrode LAM constant exponential term": 2.0*1.1,
@@ -33,6 +33,6 @@ parameter_values.update({"Positive electrode LAM constant proportional term [s-1
 
 #Plating paremeters update
 parameter_values.update({
-                        "Lithium plating kinetic rate constant [m.s-1]": 1e-09*0.0001,
+                        "Lithium plating kinetic rate constant [m.s-1]": 1e-09*0.001,
                         
                         })
