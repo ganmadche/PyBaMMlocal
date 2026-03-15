1dd6194c89af076ea99cbe082ba3b21c69ba52a4
Ganesh Madabattula <ganesh.che@iitbhu.ac.in>
2026-03-15 10:30:44 +0000

    new file for standardcycles500 parameters

---
+++ wip/std500cycles_tuneparameters.py
@@ -0,0 +1,40 @@
+#Parameters tuned for 500 cycles standard
+parameter_values.update({"Total heat transfer coefficient [W.m-2.K-1]": 10.0,})
+#thermal parameters update
+parameter_values.update({"Negative electrode OCP entropic change [V.K-1]": -0.0002,
+                         "Positive electrode OCP entropic change [V.K-1]": -0.0004,
+                         "Contact resistance [Ohm]": 0.001,
+                        })
+def ec_diffusivity(T_amb):
+    D_sj = 2.5e-22*4*10*3*2 
+    EaD = 10000
+    arrhenius = np.exp(EaD / pybamm.constants.R *(1/ 298.15 - 1/ T_amb))
+    return D_sj * arrhenius
+
+
+parameter_values.update({"EC diffusivity [m2.s-1]": ec_diffusivity})
+#sei parameters update
+parameter_values.update({"SEI kinetic rate constant [m.s-1]": 1e-12*0.002*0.5}) 
+parameter_values.update({"SEI growth activation energy [J.mol-1]": 48000.0,
+                         "SEI partial molar volume [m3.mol-1]": 9.585e-05*0.4,
+                         "SEI resistivity [Ohm.m]": 200000.0
+                         })  
+# LAM parameters update
+
+parameter_values.update({"Positive electrode LAM constant proportional term [s-1]": 0.1*0.5*1.5/3600,
+                         "Negative electrode LAM constant proportional term [s-1]": 0.2*0.5*1.5/3600,
+                         
+                         "Positive electrode LAM constant exponential term": 2.0*1.1,
+                         "Negative electrode LAM constant exponential term": 2.0*1.1,
+                         
+                         })
+
+
+
+
+
+#Plating paremeters update
+parameter_values.update({
+                        "Lithium plating kinetic rate constant [m.s-1]": 1e-09*0.001,
+                        
+                        })
